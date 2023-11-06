02be21118d02036236bb09a1b13f0fb9f1636d99
Zubair Shah <zubairsyed928@gmail.com>
2023-11-06 12:46:58 +0500

    Day 2 of #javascript30 completed. I made a Clock first time using HTML, CSS and Javascript

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,23 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+const Clock = () => {
+    const now = new Date();
+    // Seconds Hand
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360);
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    // Minutes Hand
+    const min = now.getMinutes();
+    const minDegrees = ((min / 60) * 360);
+    minuteHand.style.transform = `rotate(${minDegrees}deg)`;
+
+    // Hours Hand
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360);
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(Clock, 1000);
